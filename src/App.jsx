54753f77c9cd7fd904d016cb81a386942855bba8
dineshkumar-mb <dineshkumarmannibrundha@gmail.com>
2024-07-08 12:09:55 +0530

    router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,10 @@
+
+import{createBrowserRouter,Routerprovider}from "react-router-dom0";
+const router=createBrowserRouter([{
+  path:"/",
+  element:<h1>Hello world!</h1>
+}])
+
 const App = () => {
   return (
     <div>App</div>
