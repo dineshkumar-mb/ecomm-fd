22164622da2678cee963cd270f799096c442b87e
dineshkumar-mb <dineshkumarmannibrundha@gmail.com>
2024-07-09 11:56:29 +0530

    login, register, home page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./Components/Home"
+import Home from "./Components/Home";
+import HomeWrapper from "./Wrappers/HomeWrapper";
+import Register from "./Components/Register";
+import Login from "./Components/Login";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />
+    element: <HomeWrapper />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "register",
+        element: <Register />
+      },
+      {
+        path: "login",
+        element: <Login />
+      }
+    ]
   }
 ])
 
